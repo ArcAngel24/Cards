f28e7dbdecd913a892bac7c93f4f8e102180ff2e
ArcAngel24 <gabrif07@gmail.com>
2015-07-29 18:51:44 +0200

    Uli mag keine enums =P
    "color" wäre ein perfekter Kandidat für ein "enum class" 
    und "num" für eine enum, da man bube, dame, koenig, ass aussschreiben könnte anstatt daran zu denken, dass 1 = ass, bube = 11 usw

--- Karte.h
+++ Karte.h
@@ -5,6 +5,9 @@
 #include <string> 
 using namespace std;
 
+// enum { Ass = 1, Bube = 11, Dame = 12, Koenig = 13 };
+// enum class Color { Karo, Herz, Pik, Kreuz };
+
 class Karte {
 public:
 	Karte();
@@ -13,7 +16,7 @@ public:
 
 	void toString();
 private:
-	int num; // 1 - ass, 2-10, 11-13 - junge, königin und könig
+	int num; // 1 - ass, 2-10, 11-13 - junge, kÃ¶nigin und kÃ¶nig
 	int color; // 1 - karo, 2 - herz, 3 - pik, 4 - kreuz
 };
 
